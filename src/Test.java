b52bfac55fe3028cab7aa0389c312d4cb39afe10
Xianbo Yang <xianbo.yang@gmail.com>
2014-12-25 16:47:19 +0800

    Add test.

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		sayHello();
 	}
 
+	private static void sayHello(){
+		System.out.println("Hello world!");
+	}
 }
